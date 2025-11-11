af667abebe5ce6c036ccc090ed7bce2ab07abee6
lux <126319258+linux-giorgi@users.noreply.github.com>
2025-11-11 14:45:30 +0400

    updated pwd command

--- pwd_command/main.asm
+++ pwd_command/main.asm
@@ -1,18 +1,37 @@
-global _start 
 section .data
+	new_line db  0xa
+section .bss
+	buffer resb 256
+section .text
+	global _start
 
-buffer:
-  times 256 db 0
-  section .text
 
 _start:
-  mov rax, 79
-  mov rdi, buffer
-  mov rsi, 256
-  syscall
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, buffer
-  mov rdx, 256 
-  syscall
-  jmp $
+	mov rax, 79
+	mov rdi, buffer
+	mov rsi, 256
+	syscall
+
+	cmp rax, 0
+	js _error
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, buffer
+	mov rdx, 256
+	syscall
+	
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, new_line
+	mov rdx, 1
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+_error:
+	mov rax, 60
+	mov rdi, 1
+	syscall
